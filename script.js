d766518684ca0378b8deda730b9db74fa75964fc
suyashmhetre <mhetresuyash676@gmail.com>
2024-12-21 00:46:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,10 @@ document.addEventListener('DOMContentLoaded', function () {
         
         if (value >= 1 && value <= 100) {
             result = value * 4.71;
+              unit.textContent = value;
+        electricity.textContent = (100 * 5.16).toFixed(2);
+        distribution.textContent = (value * 1.17).toFixed(2);
+        total.textContent = `₹${result.toFixed(2)}`;
         } else if (value >= 101 && value <= 300) {
             let z = value - 100;  // Subtract 100 from the value
             let w = (100 * 5.16) + (z * 11.09) + 128 + (value * 1.17);
